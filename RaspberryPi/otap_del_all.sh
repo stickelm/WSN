f41ae151f012c47c432de9ac2103c557564917cb
xianlin <xianlin@users.noreply.github.com>
2014-07-30 17:25:33 +0800

    Update otap_del_all.sh

--- RaspberryPi/otap_del_all.sh
+++ RaspberryPi/otap_del_all.sh
@@ -4,14 +4,14 @@
 
 # scan all nodes in broadcast mode
 ./otap -scan_nodes --mode BROADCAST |  grep "Node" | awk -F "-" '{print $2,$3,$4;}' | awk '{print $2;}' | \
-    while read i
-    do
+while read i
+do
     mac=$i;
     # get boot list from the waspmote by mac address
     ./otap -get_boot_list --mode UNICAST --mac $mac | grep "PID" | sed -e "s/.*PID:\([A-Za-z0-9]\{7\}\).*/\1/" | \
-        while read i
-        do
+    while read i
+    do
         # specific task such as send/delte file, reset/start_new_program etc.
         ./otap -delete_program --mode UNICAST --mac $mac --pid $i
-        done
     done
+done
