d418906a542bf8e10fa635e928204582772b35bd
xianlin <xianlin@users.noreply.github.com>
2014-08-03 12:05:25 +0800

    Create main.py

---
+++ RaspberryPi/sensor_parser_python/main.py
@@ -0,0 +1,54 @@
+"""
+Continuously read the serial port and process IO data received from a remote XBee.
+Depedency:1)https://pypi.python.org/pypi/XBee/2.1.0
+          2)database.py
+"""
+
+#from xbee import XBee
+from database import DBConnector
+import serial
+import re
+import time
+from datetime import datetime
+#Initialization
+ser = serial.Serial('/dev/ttyUSB0', 38400)
+#xbee = XBee(ser)
+d = DBConnector()
+
+#Continuously read/parse/store packets
+while True:
+
+        """
+        read raw data from xbee
+        """
+        dataStr = ''
+        msgStr = ''
+
+        #response = xbee.wait_read_frame()
+        while(dataStr!='<'):
+                response = ser.read()
+                dataStr = str(response)
+                msgStr = msgStr + dataStr
+                #print dataStr
+
+        print msgStr
+
+        """
+        extract node id
+        """
+        m=re.search(r'NID:(\w+)',msgStr);
+        if(m):
+                nid=m.group(1)
+                print nid
+
+                """
+                store various measurement result into databse
+                """
+                Results=re.findall(r'#(\w+):(\d+[.]?[\d]?[\d]?[\d]?)',msgStr)
+                for result in Results:
+                        print result[0] #quantity
+                        print result[1] #value
+                        d.store(nid,80,result[0],result[1],result)
+
+#Termination
+ser.close()
